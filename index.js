3cfef9722656de61b0fcd22f4f8ff1cc4b76b0f7
glckfndr <obulan65@gmail.com>
2024-09-10 15:31:05 +0300

    feat: create a simple server with express

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log("Hello from express!");
+const express = require("express");
+
+const app = express();
+app.get("/", (require, responce) => {
+  responce.send("This is homepage!");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
