67089b8a90e1d0f3758dfb8b289d15084f8a35ef
glckfndr <obulan65@gmail.com>
2024-09-11 09:55:22 +0300

    feat: post request with body

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
 const express = require("express");
 
 const app = express();
-app.get("/", (require, responce) => {
-  responce.send("This is new homepage!");
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("This is new homepage!");
+});
+
+app.get("/example", (request, response, next) => {
+  response.send("This is get method!");
+});
+
+app.post("/example", (request, response, next) => {
+  let { name, email } = request.body;
+  console.log(name, email);
+  response.send("This is post method with body!");
+});
+
+app.put("/example", (request, response, next) => {
+  response.send("This is put method!");
+});
+
+app.patch("/example", (request, response, next) => {
+  response.send("This is patch method!");
+});
+
+app.delete("/example", (request, response, next) => {
+  response.send("This is delete method!");
 });
 
 app.listen(8000, () => {
